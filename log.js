078a5c1d8ba224deec0b5f56d1bc20785cec7548
LEE HYUN <71701578+hyunlee214@users.noreply.github.com>
2021-07-25 20:16:02 +0900

    로그인 ejs사용

---
+++ log.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get ('/', (req, res) => {
+  res.render('home/index');
+});
+
+app.get ('/login', (req, res) => {
+  res.render('home/login');
+});
+
+app.listen(3333, () => {
+  console.log('서버 start')
+});
