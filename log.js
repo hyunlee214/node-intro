94b5c9e1b31f2c92f33bc4fceb11474f3134f966
LEE HYUN <71701578+hyunlee214@users.noreply.github.com>
2021-07-26 21:56:39 +0900

    Update log.js

--- log.js
+++ log.js
@@ -1,10 +1,16 @@
 const express = require ('express');
 const app = express();
 
-const PORT = 3333; 
+//라우팅
+const home = require('./routes/home');
+//const PORT = 3333; 
 
+//앱 세팅
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+//use메서드를 통해 미들웨어 등록
+app.use('/', home);  
 
 app.get ('/', (req, res) => {
   res.render('home/index');
@@ -14,6 +20,9 @@ app.get ('/login', (req, res) => {
   res.render('home/login');
 });
 
-app.listen(PORT, () => {
-  console.log('서버 start')
-});
+//app.listen(PORT, () => {
+//  console.log('서버 start')
+//});
+
+// app.listen() 모듈화 =>
+module.exports = app;
