3a4c479f63bfb4eb673a206ac2a343ef3f74c028
LEE HYUN <71701578+hyunlee214@users.noreply.github.com>
2021-08-02 22:34:28 +0900

    인덱스파일 업데이트 [models>index.js

--- sequelize/index.js
+++ sequelize/index.js
@@ -1,37 +1,17 @@
-'use strict';
-
-const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.json')[env];
+
+//시퀄라이즈 인스턴스화
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+//db라는 객체를 만들어 모듈로 사용
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
+//시퀄라이즈 패키지와 시퀄라이즈 인스턴스를 db에 각각 넣어서 사용
 db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 module.exports = db;
