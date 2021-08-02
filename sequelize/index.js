ea68a5b1f0a232539fe24582d91669c8a43886aa
LEE HYUN <71701578+hyunlee214@users.noreply.github.com>
2021-08-02 23:58:26 +0900

    'models>index' 에 user,comment추가

--- sequelize/index.js
+++ sequelize/index.js
@@ -14,4 +14,7 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.User = require('./user')(sequelize, Sequelize);
+db.Comment = require('./comment')(sequelize, Sequelize);
+
 module.exports = db;
