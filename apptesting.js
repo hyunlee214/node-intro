ca6c88bc4c9d95b573633e0361770843d5b61b02
LEE HYUN <71701578+hyunlee214@users.noreply.github.com>
2021-08-02 21:38:26 +0900

    express테스팅

--- apptesting.js
+++ apptesting.js
@@ -6,6 +6,17 @@ app.get ('/', (req, res) => {
   res.send('testing express');
 });
 
+app.get ('/users', (req, res) => {
+  res.send('test express');
+  
+app.post('/', (req, res) => {
+ 
+});
+  
+app.delete('/', (req, res) => {
+  
+});
+
 module.exports = app;
 
 
